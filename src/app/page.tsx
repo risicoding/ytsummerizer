6db1252e44963f3a7d87b5ac1c6cec853a35b437
Risi <risimn7@gmail.com>
2025-01-15 19:59:00 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <Card>
+      <CardHeader>T3 ap template</CardHeader>
+      <CardContent>
+        <Button>Get started</Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default page;
